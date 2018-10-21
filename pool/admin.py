2327e35434482478e5860b2dea1f5b78dd69c950
Charles Swain <cswain1@memphis.edu>
2018-10-21 14:01:27 -0500

    changed models

--- pool/admin.py
+++ pool/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Bet, Game, Team
+
+admin.site.register(Bet)
+admin.site.register(Game)
+admin.site.register(Team)
